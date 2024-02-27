aeaf59873fa1de600fad953a1831151465747bfb
zhakhangers <zhakhanger.shaidullov@gmail.com>
2024-02-27 18:11:16 +0600

    Generate version number automatically

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,9 +18,10 @@ import (
 	"zhakhangers.net/greenlight/internal/mailer"
 )
 
-const version = "1.0.0"
-
-var buildTime string
+var (
+	buildTime string
+	version   string
+)
 
 type config struct {
 	port int
